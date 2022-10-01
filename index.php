3d48aa2be4362e0c732178ef43310f6b032219b2
Omar Tarek <omartarek711@gmail.com>
2022-10-01 19:24:17 +0200

    Create routes in our index page

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-
-
 require_once __DIR__ . '/vendor/autoload.php';
 
+use app\core\Application;
 
-$app = new \app\core\Application();
+
+
+$app = new Application();
+
+$app->router->get('/', function() {
+    return 'Hello From root rout!';
+});
+
+$app->router->get('/contact', function() {
+    return 'Contact Page :)';
+});
+
+$app->run();
