d21215fa8fa9fde649bcfe0b6e4207046fba18c1
Omar Tarek <omartarek711@gmail.com>
2022-10-01 19:38:30 +0200

    Create a request class

---
+++ core/Request.php
@@ -0,0 +1,29 @@
+<?php
+namespace app\core;
+
+/**
+ * Class Request
+ * 
+ * @author: Omar Tarek <omartarek611@outlook.com>
+ * @package app\core
+ */
+class Request
+{
+  public function getPath()
+  {
+    $path = $_SERVER['REQUEST_URI'] ?? '/';
+    $position = strpos($path, '?');
+
+    if (!$position) {
+      return $path;
+    }
+    $path = substr($path,0,$position);
+    
+    return $path;
+  }
+
+  public function getMethod()
+  {
+    
+  }
+}
