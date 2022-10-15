3e6a714efafea378360bec2e8e3abf1bc8c19ff4
Omar Tarek <omartarek711@gmail.com>
2022-10-15 23:42:20 +0200

    Create auth controller with routes

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use app\controllers\AuthController;
 use app\controllers\SiteController;
 use app\core\Application;
 
@@ -10,5 +11,11 @@ $app->router->get('/', [SiteController::class, 'home']);
 
 $app->router->get('/contact', [SiteController::class, 'contact']);
 $app->router->post('/contact', [SiteController::class, 'handelContact']);
+
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
+
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->run();
