22d1a409b5bc472d72edbf046f0e68f30ccaa50d
Omar Tarek <omartarek711@gmail.com>
2022-10-15 23:40:01 +0200

    Change a function name
    Change the 'getMethod' function name  to 'method'

--- core/Request.php
+++ core/Request.php
@@ -23,7 +23,7 @@ class Request
     return $path;
   }
 
-  public function getMethod()
+  public function method()
   {
     return strtolower($_SERVER['REQUEST_METHOD']);
   }
@@ -31,12 +31,12 @@ class Request
   public function getBody()
   {
     $body = [];
-    if ($this->getMethod() == 'get') {
+    if ($this->method() == 'get') {
       foreach ($_GET as $key => $value) {
         $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
       }
     }
-    if ($this->getMethod() == 'post') {
+    if ($this->method() == 'post') {
       foreach ($_POST as $key => $value) {
         $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
       }
